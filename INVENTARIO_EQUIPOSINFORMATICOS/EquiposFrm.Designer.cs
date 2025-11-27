800402ef8cc99168fec8b2455603f1e24c568233
Marco <75067868@idexperujapon.edu.pe>
2025-11-26 19:31:40 -0500

    cambios varios

--- INVENTARIO_EQUIPOSINFORMATICOS/EquiposFrm.Designer.cs
+++ INVENTARIO_EQUIPOSINFORMATICOS/EquiposFrm.Designer.cs
@@ -53,7 +53,7 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(371, 238);
+            this.dataGridView1.Location = new System.Drawing.Point(297, 51);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
             this.dataGridView1.TabIndex = 2;
---
+++ INVENTARIO_EQUIPOSINFORMATICOS/conexion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INVENTARIO_EQUIPOSINFORMATICOS
+{
+    internal class conexion
+    {
+        SqlConnection cn = new SqlConnection("Data Source=192.168.10.2;DataBase=INVENTARIO_EQUIPOSINFORMATICOS;User=sa;Password=Hyp3rn10Pr0_;TrustServerCertificate=true");
+        public void conectar()
+        {
+            if (cn.State != System.Data.ConnectionState.Open)
+            {
+                cn.Open();
+            }
+        }
+        public void desconectar()
+        {
+            if (cn.State == System.Data.ConnectionState.Open)
+            {
+                cn.Close();
+            }
+        }
+        public SqlConnection getConexion()
+        {
+            return cn;
+        }
+
+    }
+}
